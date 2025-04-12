9b4f2d680fb6c89cda5327ea309193f382d89beb
lokipaws45 <lokipaws.orders@gmail.com>
2025-04-12 14:44:03 +0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,2 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Main entry point placeholder
+console.log("App Loaded");
