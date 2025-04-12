b195f23522f273b182631b87598e1352335e538c
lokipaws45 <lokipaws.orders@gmail.com>
2025-04-12 18:38:52 +0400

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css';
+import Home from './pages/Home';
+import Admin from './pages/Admin';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </Router>
+  );
+}
