1175c2302cab6a77484a8060e55cf36e1b953ed4
lokipaws45 <lokipaws.orders@gmail.com>
2025-04-12 15:26:24 +0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,17 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCWOlRZV38KcvpDg1rMLyONOUmN5eUu1Qs",
+  authDomain: "lokipawsorders.firebaseapp.com",
+  projectId: "lokipawsorders-b4470",
+  storageBucket: "lokipawsorders-b4470.appspot.com",
+  messagingSenderId: "977549459382",
+  appId: "1:977549459382:web:c25e3d44e6ca9e580a0b17"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-export const provider = new GoogleAuthProvider();
 export const storage = getStorage(app);
-export const db = getFirestore(app);
